5a496f0cd8851c51516878d93209d529194a6aae
bornkiller <hjj491229492@hotmail.com>
2016-10-03 17:34:37 +0800

    feature: ui-router模板初步支持热更新。

--- src/layout/layout.route.js
+++ src/layout/layout.route.js
@@ -2,6 +2,7 @@
  * @description - layout router config
  * @author - bornkiller <hjj491229492@hotmail.com>
  */
+import { SidebarController } from './flow/sidebar.controller';
 
 // router rule declare
 export const LayoutRoute = [
@@ -22,7 +23,9 @@ export const LayoutRoute = [
         template: require('./flow/navbar.html')
       },
       'sidebar': {
-        template: require('./flow/sidebar.html')
+        template: require('./flow/sidebar.html'),
+        controller: SidebarController,
+        controllerAs: 'vm'
       },
       'core': {
         template: require('./flow/core.html')
@@ -31,9 +34,23 @@ export const LayoutRoute = [
   }
 ];
 
-// router config implement
-export  /* @ngInject */  function $layoutRouterConfig($stateProvider) {
-  [...LayoutRoute].forEach((route) => {
-    $stateProvider.state(route);
+if (module.hot) {
+  let element = angular.element(document.body); // eslint-disable-line
+  
+  module.hot.accept(['./flow/navbar.html', './flow/sidebar.html', './flow/core.html'], function () {
+    let $injector = element.injector();
+    
+    if (!$injector) return;
+    
+    let $rootScope = $injector.get('$rootScope');
+    let $state = $injector.get('$state');
+    let target = $state.get('application');
+    
+    target.views.navbar.template = require('./flow/navbar.html');
+    target.views.sidebar.template = require('./flow/sidebar.html');
+    target.views.core.template = require('./flow/core.html');
+    
+    $state.reload('application');
+    $rootScope.$apply();
   });
 }
