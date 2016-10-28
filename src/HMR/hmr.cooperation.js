f26f463679fce4d690d2c96c63f7ac4857840832
黄健 <jian.huang03@ele.me>
2016-10-28 15:28:12 +0800

    feature: 添加HMR初步热更新实现。

--- src/HMR/hmr.cooperation.js
+++ src/HMR/hmr.cooperation.js
@@ -5,18 +5,15 @@
 'use strict';
 
 import omit from 'lodash/omit';
+import has from 'lodash/has';
 import { Subject } from 'rxjs/Subject';
-// import { mapTo } from 'rxjs/operator/mapTo';
 
+/* eslint-disable angular/document-service, angular/angularelement */
 export /* @ngInject */ function HMRProvider() {
   const Storage = new Map();
 
   this.register = register;
   this.pick = pick;
-
-  this.$get = [function () {
-    return {notify};
-  }];
 
   // name = state.name + views.name + template / controller
   function register(name) {
@@ -27,17 +24,64 @@ export /* @ngInject */ function HMRProvider() {
     return Storage.get(name);
   }
 
-  function notify(name, module) {
-    let subject = Storage.get(name);
+  this.$get = ['$injector', '$compile', '$state', function ($injector, $compile, $state) {
+    return {
+      notify
+    };
 
-    subject.next(module);
-  }
+    function notify(name, hotModule) {
+      let subject = Storage.get(name);
+      let [stateName, viewName, hotModuleType] = name.split('_');
+
+      // 第一步先实现精确匹配
+      if ($state.is(stateName)) {
+        hotModuleType === 'template' ? hotUpdateTemplate(viewName, hotModule) : hotUpdateController(viewName, hotModule);
+      }
+
+      // 此处仅修改router声明, reload的时候才会生效,使之符合HMR原则
+      subject.next(hotModule);
+    }
+
+    function hotUpdateTemplate(viewName, template) {
+      let selector = `[ui-view=${viewName}]`;
+      let target = document.querySelector(selector);
+      let scope = angular.element(target).scope();
+      let middleware = $compile(template)(scope);
+
+      $(selector).replaceWith(middleware);
+      scope.$apply();
+    }
+
+    function hotUpdateController(viewName, controller) {
+      let selector = `[ui-view=${viewName}]`;
+      let target = document.querySelector(selector);
+      let scope = angular.element(target).scope();
+      let prevVM = scope.vm;
+      let nextVM = $injector.instantiate(controller, {$scope: scope});
+      let toString = Object.prototype.toString;
+
+      // 假设所有关联属性在constructor内部声明,变量类型不变
+      angular.forEach(nextVM, (value, property) => {
+        if (!has(prevVM, property) || toString.call(prevVM[property]) !== toString.call(nextVM[property])) {
+          prevVM[property] = value;
+        }
+      });
+
+      angular.forEach(Object.getOwnPropertyNames(nextVM.__proto__), (value, property) => {
+        if (property !== 'constructor') {
+          prevVM.__proto__[property] = nextVM.__proto__[property];
+        }
+      });
+
+      scope.$apply();
+    }
+  }];
 }
 
 export /* @ngInject */ function HMRStateProviderConfig($stateProvider, $hmrProvider) {
-  $stateProvider.decorator('views', function (state, parent) {
+  $stateProvider.decorator('views', function (state, $delegate) {
     let target = {};
-    let views = parent(state);
+    let views = $delegate(state);
 
     angular.forEach(views, (config, viewName) => {
       let middleware = omit(config, ['template', 'controller']);
