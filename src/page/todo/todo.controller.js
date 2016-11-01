15899f797e20f0c0a4f8002f90c9883713cba5aa
黄健 <jian.huang03@ele.me>
2016-11-01 19:16:27 +0800

    feature: 添加弹窗。

--- src/page/todo/todo.controller.js
+++ src/page/todo/todo.controller.js
@@ -8,8 +8,9 @@ import * as TodoActions from './action.creator';
 
 /* @ngInject */
 export class TodoController {
-  constructor($q, $scope, $ngRedux) {
+  constructor($q, $scope, $ngRedux, $uibModal) {
     this.$q = $q;
+    this.$uibModal =$uibModal;
 
     let disconnect = $ngRedux.connect((state) => ({
       status: state.todo.view.status,
@@ -27,4 +28,22 @@ export class TodoController {
     // validate person here
     this.requestCreateTodo(person);
   }
+
+  displayPoemModal() {
+    this.$uibModal.open({
+      template: require('./modal.html'),
+      identity: 'HM_MODAL_YOUTH'
+    });
+  }
+}
+
+if (module.hot) {
+  module.hot.accept(['./modal.html'], function () {
+    let element = angular.element(document.body);
+    let $injector = element.injector();
+    let $hmr = $injector.get('$hmr');
+    let template = require('./modal.html');
+
+    $hmr.update('HM_MODAL_YOUTH', template);
+  });
 }
