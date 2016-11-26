0f321dc26a579dd4eba44a2831f987359c3eda89
bornkiller <hjj491229492@hotmail.com>
2016-11-26 23:10:52 +0800

    temp.

---
+++ src/page/love/love.controller.js
@@ -0,0 +1,18 @@
+/**
+ * @description - love feature controller
+ * @author - bornkiller <hjj491229492@hotmail.com>
+ */
+
+'use strict';
+
+export class LoveController {
+  /* @ngInject */
+  constructor($q) {
+    this.$q = $q;
+  }
+  
+  syncLoveConnection() {
+  }
+}
+
+LoveController.ng_hmr_identity = 'page-love-love-controller';
